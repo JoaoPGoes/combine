d485b3609e9a2536fe181fdbb2736c8f6b0a2214
João Pedro Goes <joaopedro.goes13@gmail.com>
2021-07-04 18:03:26 -0300

    initial settings

---
+++ backend_combine/src/main/java/net/ufjnet/combinebackend/BackendCombineApplication.java
@@ -0,0 +1,13 @@
+package net.ufjnet.combinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCombineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCombineApplication.class, args);
+	}
+
+}
