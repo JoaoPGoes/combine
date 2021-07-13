0149287c5394d5112f8154d6a76a5097631a96f2
João Pedro Goes <joaopedro.goes13@gmail.com>
2021-07-13 06:41:43 -0300

    feat: implements SWAGGER

--- backend_combine/src/main/java/net/ufjnet/combinebackend/controllers/CombinerController.java
+++ backend_combine/src/main/java/net/ufjnet/combinebackend/controllers/CombinerController.java
@@ -1,5 +1,7 @@
 package net.ufjnet.combinebackend.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.ufjnet.combinebackend.dtos.CombinerDTO;
 import net.ufjnet.combinebackend.models.Combiner;
 import net.ufjnet.combinebackend.services.CombinerService;
@@ -18,12 +20,14 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/v1/combine/user")
+@Tag(name = "Endpoint de Combiner(Users)")
 public class CombinerController {
 	
 	@Autowired
 	private CombinerService service;
 
 	@GetMapping
+	@Operation(summary = "Busca todos os usuarios")
 	public ResponseEntity<CollectionModel<CombinerDTO>> buscarTodos(
 		@RequestParam(value="page", defaultValue = "0") int page,
 		@RequestParam(value="limit", defaultValue = "12") int limit,
@@ -44,15 +48,8 @@ public class CombinerController {
 		return ResponseEntity.ok(CollectionModel.of(pages));
 	}
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<CombinerDTO> insert(@RequestBody CombinerDTO objBody) {
-		CombinerDTO objDTO = service.save(objBody);
-		objDTO.add(linkTo(methodOn(CombinerController.class).service.findById(objDTO.getId())).withSelfRel());
-		return ResponseEntity.ok(objDTO);
-	}
-
 	@GetMapping("/searchById/{id}")
+	@Operation(summary = "Busca usuario por ID")
 	public ResponseEntity<CombinerDTO> searchById(@PathVariable String id) {
 		CombinerDTO objDTO = service.findById(id);
 		objDTO.add(linkTo(methodOn(CombinerController.class).searchById(id)).withSelfRel());
@@ -60,6 +57,7 @@ public class CombinerController {
 	}
 
 	@GetMapping("/searchByUsername/{username}")
+	@Operation(summary = "Busca usuario por USERNAME")
 	public ResponseEntity<CombinerDTO> searchByUsername(@PathVariable String username) {
 		CombinerDTO objDTO = service.findByUsername(username);
 		objDTO.add(linkTo(methodOn(CombinerController.class).searchByUsername(username)).withSelfRel());
@@ -67,13 +65,24 @@ public class CombinerController {
 	}
 
 	@GetMapping("/searchByEmail/{email}")
+	@Operation(summary = "Busca usuario por EMAIL")
 	public ResponseEntity<CombinerDTO> searchByEmail(@PathVariable String email) {
 		CombinerDTO objDTO = service.findByEmail(email);
 		objDTO.add(linkTo(methodOn(CombinerController.class).searchByEmail(email)).withSelfRel());
 		return ResponseEntity.ok(objDTO);
 	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "Insere um usuario")
+	public ResponseEntity<CombinerDTO> insert(@RequestBody CombinerDTO objBody) {
+		CombinerDTO objDTO = service.save(objBody);
+		objDTO.add(linkTo(methodOn(CombinerController.class).service.findById(objDTO.getId())).withSelfRel());
+		return ResponseEntity.ok(objDTO);
+	}
+
 	@PutMapping("/edit/{id}")
+	@Operation(summary = "Altera um usuario por ID")
 	public ResponseEntity<CombinerDTO> update(@PathVariable String id, @RequestBody Combiner obj) {
 		CombinerDTO objDTO = service.update(obj);
 		objDTO.add(linkTo(methodOn(CombinerController.class).searchById(id)).withSelfRel());
@@ -81,6 +90,7 @@ public class CombinerController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Deleta um usuario por ID")
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		if (!service.verifyId(id)) {
 			return ResponseEntity.notFound().build();
